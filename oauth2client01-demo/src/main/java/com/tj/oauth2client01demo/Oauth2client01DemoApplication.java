6d6f38ed5252af3534c6fc4badc5751227fbb7e0
leonard.liuz <leonard.liuz@sohu.com>
2022-07-26 18:18:37 +0800

    oauth2client01-demo,first commit: application.pro~ pom.xml.

---
+++ oauth2client01-demo/src/main/java/com/tj/oauth2client01demo/Oauth2client01DemoApplication.java
@@ -0,0 +1,13 @@
+package com.tj.oauth2client01demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2client01DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2client01DemoApplication.class, args);
+    }
+
+}
